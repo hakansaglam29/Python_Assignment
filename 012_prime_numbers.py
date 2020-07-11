6e4211d14f659068781a14c1d317534039b95500
Hakan <user@Users-iMac.local>
2020-07-11 14:48:53 +0200

    second commit

--- 012_prime_numbers.py
+++ 012_prime_numbers.py
@@ -1,5 +1,16 @@
 number = input("Please enter a number: ")
-sum = 0
 while not number.isdecimal():
     print("It is an invalid entry. Don't use non-numeric, float, or negative values!")
     number = input("Please enter a number: ")
+if int(number)==2:
+    print("{} is a prime number".format(number))
+elif int(number)==1 or int(number)>2 and int(number)%2==0:
+     print("{} is not a prime number".format(number))  
+elif int(number)>2:
+    for i in range(3,(int(number)+1),2):
+        if int(number)%i==0:
+            if int(number)==i:
+                print("{} is a prime number".format(number))
+            else:
+                print("{} is not a prime number".format(number))
+                break
